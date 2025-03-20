4c3504d358e71983a740ee7788cd7f59025ff784
nskydiving <n.skydiving@gmail.com>
2025-03-20 18:14:04 +0900

    lint エラーの修正

--- app/ui/invoices/search.tsx
+++ app/ui/invoices/search.tsx
@@ -5,12 +5,12 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
     console.log(term);
-    const params = new URLSearchParams(searchParams);
-    const pathname = usePathname();
-    const { replace } = useRouter();
 
     params.set('page', '1');
     
